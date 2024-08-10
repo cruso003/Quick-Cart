723673f2f73f0071b0f500360fef062d04999fd9
Nwachiemu <emuchaynwachi@gmail.com>
2024-08-10 21:53:38 +0100

    correct file path

--- quick-cart-backend/src/routes/swagger.route.js
+++ quick-cart-backend/src/routes/swagger.route.js
@@ -1,17 +1,28 @@
-// src/routes/swagger.js
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
+import { fileURLToPath } from 'url';
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const router = express.Router();
 
 // Path to the openapi.yaml file
-const swaggerDocument = yaml.load(fs.readFileSync(path.resolve(__dirname, '../../../docs/api/openapi.yaml'), 'utf8'));
+const yamlFilePath = path.resolve(__dirname, '../../../docs/api/openapi.yaml');
 
-// Serve the OpenAPI documentation
-router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+try {
+  if (fs.existsSync(yamlFilePath)) {
+    const swaggerDocument = yaml.load(fs.readFileSync(yamlFilePath, 'utf8'));
+
+    // Serve the OpenAPI documentation
+    router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  } else {
+    console.error('Error: openapi.yaml file not found at path:', yamlFilePath);
+  }
+} catch (err) {
+  console.error('Error reading or parsing openapi.yaml file:', err);
+}
 
 export default router;
