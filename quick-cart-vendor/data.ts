d2e2410462873a8cb375e62057f9a5ec0737f15e
Nwachiemu <emuchaynwachi@gmail.com>
2024-08-11 04:36:36 +0100

    feat: Set up initial project structure with global styles, routing, and essential components
    - Installed necessary packages: react-router-dom, react-toastify etc
    - Created global.scss for global styles
    - Added NavBar, Menu, Footer, and NotFound components
    - Implemented basic layout with routing using react-router-dom
    - Integrated toast notifications with react-toastify

---
+++ quick-cart-vendor/data.ts
@@ -0,0 +1,34 @@
+export const menu = [
+    {
+      id: 1,
+      title: "main",
+      listItems: [
+        {
+          id: 1,
+          title: "Dashboard",
+          url: "/",
+          icon: "dashboard.svg",
+        },
+        {
+          id: 2,
+          title: "Profile",
+          url: "/profile",
+          icon: "profile.svg",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "management",
+      listItems: [
+        {
+          id: 1,
+          title: "Products",
+          url: "/products",
+          icon: "product.svg",
+        }
+        
+      ],
+    },
+  ];
+  
--- quick-cart-vendor/src/App.tsx
+++ quick-cart-vendor/src/App.tsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './styles/global.scss';
+import Navbar from './components/navbar/NavBar';
+import Menu from './components/menu/Menu';
+import Footer from './components/footer/Footer';
+import NotFound from './components/notFound/NotFound';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Layout = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="main">
+      <Navbar />
+      <div className="container">
+        <div className="menuContainer">
+          <Menu />
+        </div>
+        <div className="contentContainer">
+          <Outlet />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+        <Layout />
+    ),
+    children: [
+      
+    ],
+  },
+  
+  {
+    path: "*",
+    element: <NotFound/>
+  }
+  
+]);
+function App() {
+  return (
+    <><RouterProvider router={router} /><ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored" /></>
   )
+  
 }
 
 export default App
