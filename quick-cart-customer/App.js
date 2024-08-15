10258f1819e3f0f36ad746bc497c66cd1e40af80
Nwachiemu <emuchaynwachi@gmail.com>
2024-08-15 08:30:57 +0100

    created the authentication context and usuage login screen

--- quick-cart-customer/App.js
+++ quick-cart-customer/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import HomeStack from "./navigation/stackNavigation/HomeStack";
 import { DeliveryAddressProvider } from "./context/DeliveryAddress";
+import { AuthProvider } from "./context/auth";
 
 export default function App() {
   const [initializing, setInitializing] = useState(true);
@@ -13,10 +14,12 @@ export default function App() {
   if (initializing) return null;
 
   return (
+    <AuthProvider>
     <NavigationContainer>
       <DeliveryAddressProvider>
       <HomeStack />
       </DeliveryAddressProvider>
     </NavigationContainer>
+    </AuthProvider>
   );
 }
